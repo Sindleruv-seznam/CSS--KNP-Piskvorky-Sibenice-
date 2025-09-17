f165f9959a525fb9e633a1700869c9e4959dbacf
Matouš Šindler <sindlerm24@spstrutnov.cz>
2025-09-17 11:35:52 +0200

    Input, začátek textu

--- KNP, Piskvorky, Sibenice/KNP, Piskvorky, Sibenice/Program.cs
+++ KNP, Piskvorky, Sibenice/KNP, Piskvorky, Sibenice/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace KNP__Piskvorky__Sibenice
+﻿using System.Diagnostics;
+
+namespace KNP__Piskvorky__Sibenice
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //INPUT A DEKLARACE PROMENNYCH
+            Random random = new Random();
+            int VyberComp = random.Next(1, 4);
+            Console.WriteLine("Vyberte si jedno z následujících: kámen, nůžky, papír");
+            string VyberUser = Console.ReadLine();
+
+            //VYPIS TEXTU
+            Console.WriteLine($"Uživatel vybral {VyberUser}");
+            if (VyberComp == 1)
+            {
+                Console.WriteLine("Počítač vybral kámen")
+            }
+            else if (VyberComp == 2)
+            { 
+            
+            }
         }
     }
 }
