cc3c92859d4d1054c942052acde8478b49ccf5b5
Matouš Šindler <sindlerm24@spstrutnov.cz>
2025-09-17 11:01:50 +0200

    Soubory

---
+++ KNP, Piskvorky, Sibenice/KNP, Piskvorky, Sibenice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KNP__Piskvorky__Sibenice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
