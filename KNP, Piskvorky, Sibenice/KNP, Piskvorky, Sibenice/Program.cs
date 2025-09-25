5c667be7c101b2c206531ed3d2941f24204612bb
Matouš Šindler <sindlerm24@spstrutnov.cz>
2025-09-25 09:36:49 +0200

    While loop KNP

--- KNP, Piskvorky, Sibenice/KNP, Piskvorky, Sibenice/Program.cs
+++ KNP, Piskvorky, Sibenice/KNP, Piskvorky, Sibenice/Program.cs
@@ -6,72 +6,87 @@ namespace KNP__Piskvorky__Sibenice
     {
         static void Main(string[] args)
         {
-            //VYBER POCITACE
-            Random random = new Random();
-            int VyberComp = random.Next(1, 4);
-            int VyberUser = 0;
+            bool Running = true;
+            while (Running == true)
+            {
+                //VYBER POCITACE
+                Random random = new Random();
+                int VyberComp = random.Next(1, 4);
+                int VyberUser = 0;
 
-            //INPUT
-            Console.WriteLine("Vyberte si jedno z následujících: kámen, nůžky, papír");
-            string VolbaUser = Console.ReadLine();
+                //INPUT
+                Console.WriteLine("Vyberte si jedno z následujících: kámen, nůžky, papír");
+                string VolbaUser = Console.ReadLine();
 
-            //PREVOD VYBERU UZIVATELE NA CISLO
-            if (VolbaUser == "kámen" || VolbaUser == "kamen" || VolbaUser == "Kámen" || VolbaUser == "Kamen")
-            {
-                VyberUser = (1);
-            }
-            else if (VolbaUser == "nůžky" || VolbaUser == "nuzky" || VolbaUser == "nůzky" || VolbaUser == "nužky" || VolbaUser == "Nužky" || VolbaUser == "Nůžky" || VolbaUser == "Nuzky" || VolbaUser == "Nůzky")
-            {
-                VyberUser = (2);
-            }
-            else if (VolbaUser == "papir" || VolbaUser == "papír" || VolbaUser == "Papir" || VolbaUser == "Papír")
-            {
-                VyberUser = (3);
-            }
+                //PREVOD VYBERU UZIVATELE NA CISLO
+                if (VolbaUser == "kámen" || VolbaUser == "kamen" || VolbaUser == "Kámen" || VolbaUser == "Kamen")
+                {
+                    VyberUser = (1);
+                }
+                else if (VolbaUser == "nůžky" || VolbaUser == "nuzky" || VolbaUser == "nůzky" || VolbaUser == "nužky" || VolbaUser == "Nužky" || VolbaUser == "Nůžky" || VolbaUser == "Nuzky" || VolbaUser == "Nůzky")
+                {
+                    VyberUser = (2);
+                }
+                else if (VolbaUser == "papir" || VolbaUser == "papír" || VolbaUser == "Papir" || VolbaUser == "Papír")
+                {
+                    VyberUser = (3);
+                }
 
-            //VYPIS TEXTU
-            Console.WriteLine($"Uživatel vybral {VolbaUser}");
-            if (VyberComp == 1)
-            {
-                Console.WriteLine("Počítač vybral kámen");
-            }
-            else if (VyberComp == 2)
-            {
-                Console.WriteLine("Počítač vybral nůžky");
-            }
-            else if (VyberComp == 3)
-            {
-                Console.WriteLine("Počítač vybral papír");
-            }
+                //VYPIS TEXTU
+                Console.WriteLine($"Uživatel vybral {VolbaUser}");
+                if (VyberComp == 1)
+                {
+                    Console.WriteLine("Počítač vybral kámen");
+                }
+                else if (VyberComp == 2)
+                {
+                    Console.WriteLine("Počítač vybral nůžky");
+                }
+                else if (VyberComp == 3)
+                {
+                    Console.WriteLine("Počítač vybral papír");
+                }
 
-            //POSOUZENI VITEZE
-            if (VyberComp == VyberUser)
-            {
-                Console.WriteLine("Remíza");
-            }
-            else if (VyberComp == 1 & VyberUser == 2)
-            {
-                Console.WriteLine("Počítač vyhrál");
-            }
-            else if (VyberComp == 1 & VyberUser == 3)
-            {
-                Console.WriteLine("Uživatel vyhrál");
-            }
-            else if (VyberComp == 2 & VyberUser == 1)
-            {
-                Console.WriteLine("Uživatel vyhrál");
-            }
-            else if (VyberComp == 2 & VyberUser == 3)
-            {
-                Console.WriteLine("Počítač vyhrál");
-            }
-            else if (VyberComp == 3 & VyberUser == 1)
-            {
-                Console.WriteLine("Počítač vyhrál");
-            }
-            else if (VyberComp == 3 & VyberUser == 2)
-            {
-                Console.WriteLine("Uživatel vyhrál");
+                //POSOUZENI VITEZE
+                if (VyberComp == VyberUser)
+                {
+                    Console.WriteLine("Remíza");
+                }
+                else if (VyberComp == 1 & VyberUser == 2)
+                {
+                    Console.WriteLine("Počítač vyhrál");
+                }
+                else if (VyberComp == 1 & VyberUser == 3)
+                {
+                    Console.WriteLine("Uživatel vyhrál");
+                }
+                else if (VyberComp == 2 & VyberUser == 1)
+                {
+                    Console.WriteLine("Uživatel vyhrál");
+                }
+                else if (VyberComp == 2 & VyberUser == 3)
+                {
+                    Console.WriteLine("Počítač vyhrál");
+                }
+                else if (VyberComp == 3 & VyberUser == 1)
+                {
+                    Console.WriteLine("Počítač vyhrál");
+                }
+                else if (VyberComp == 3 & VyberUser == 2)
+                {
+                    Console.WriteLine("Uživatel vyhrál");
+                }
+                Console.WriteLine("Přejete si hrát znovu? (Y/N)");
+                string HratZnovu = Console.ReadLine();
+                if (HratZnovu == "Y")
+                {
+                    Running = true;
+                }
+                if (HratZnovu == "N")
+                {
+                    Running = false;
+                }
+
             }
         }
     }
---
+++ KNP, Piskvorky, Sibenice/šibenica/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace šibenica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
