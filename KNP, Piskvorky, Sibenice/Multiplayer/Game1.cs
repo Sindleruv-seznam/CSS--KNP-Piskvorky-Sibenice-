50609e71dc783d4fc7cc72a3b2a7abff5898821c
Matouš Šindler <sindlerm24@spstrutnov.cz>
2025-10-23 08:15:18 +0200

    Multiplayer hotov
    Ovládání WASD IJKL a šipky

--- KNP, Piskvorky, Sibenice/Multiplayer/Game1.cs
+++ KNP, Piskvorky, Sibenice/Multiplayer/Game1.cs
@@ -9,7 +9,16 @@ namespace Multiplayer
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private Texture2D _pixel;
-        private Vector2 _pos = new(100, 100);
+
+        // Red square controlled by WASD
+        private Vector2 _posWASD = new(100, 100);
+        // Green square controlled by IJKL
+        private Vector2 _posIJKL = new(200, 100);
+        // Blue square controlled by Arrow keys
+        private Vector2 _posArrows = new(300, 100);
+
+        private const int SquareSize = 50;
+        private const float Speed = 3f;
 
         public Game1()
         {
@@ -31,10 +40,24 @@ namespace Multiplayer
                 Exit();
 
             var k = Keyboard.GetState();
-            if (k.IsKeyDown(Keys.W)) _pos.Y -= 3;
-            if (k.IsKeyDown(Keys.S)) _pos.Y += 3;
-            if (k.IsKeyDown(Keys.A)) _pos.X -= 3;
-            if (k.IsKeyDown(Keys.D)) _pos.X += 3;
+
+            // WASD control the red square
+            if (k.IsKeyDown(Keys.W)) _posWASD.Y -= Speed;
+            if (k.IsKeyDown(Keys.S)) _posWASD.Y += Speed;
+            if (k.IsKeyDown(Keys.A)) _posWASD.X -= Speed;
+            if (k.IsKeyDown(Keys.D)) _posWASD.X += Speed;
+
+            // IJKL control the green square
+            if (k.IsKeyDown(Keys.I)) _posIJKL.Y -= Speed;
+            if (k.IsKeyDown(Keys.K)) _posIJKL.Y += Speed;
+            if (k.IsKeyDown(Keys.J)) _posIJKL.X -= Speed;
+            if (k.IsKeyDown(Keys.L)) _posIJKL.X += Speed;
+
+            // Arrow keys control the blue square
+            if (k.IsKeyDown(Keys.Up)) _posArrows.Y -= Speed;
+            if (k.IsKeyDown(Keys.Down)) _posArrows.Y += Speed;
+            if (k.IsKeyDown(Keys.Left)) _posArrows.X -= Speed;
+            if (k.IsKeyDown(Keys.Right)) _posArrows.X += Speed;
 
             base.Update(gameTime);
         }
@@ -42,9 +65,16 @@ namespace Multiplayer
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+
             _spriteBatch.Begin();
-            _spriteBatch.Draw(_pixel, new Rectangle((int)_pos.X, (int)_pos.Y, 50, 50), Color.Red);
+            // Red: WASD
+            _spriteBatch.Draw(_pixel, new Rectangle((int)_posWASD.X, (int)_posWASD.Y, SquareSize, SquareSize), Color.Red);
+            // Green: IJKL
+            _spriteBatch.Draw(_pixel, new Rectangle((int)_posIJKL.X, (int)_posIJKL.Y, SquareSize, SquareSize), Color.Green);
+            // Blue: Arrow keys
+            _spriteBatch.Draw(_pixel, new Rectangle((int)_posArrows.X, (int)_posArrows.Y, SquareSize, SquareSize), Color.Blue);
             _spriteBatch.End();
+
             base.Draw(gameTime);
         }
     }
